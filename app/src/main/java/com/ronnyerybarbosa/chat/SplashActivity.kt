d81893174b902aff7b39fea08b7724ac9de02edb
Ronnyery Barbosa <ronnyerybarbosa@gmail.com>
2019-05-06 08:44:20 -0300

    casa

---
+++ app/src/main/java/com/ronnyerybarbosa/chat/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.ronnyerybarbosa.chat
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import org.jetbrains.anko.startActivity
+
+class SplashActivity: AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (FirebaseAuth.getInstance().currentUser == null)
+        {
+            startActivity<SingInActivity>()
+        }
+    }
+}
